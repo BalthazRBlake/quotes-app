758d0e66332e358d6ae0de3ae13056f205cf78d0
FHHF <fabianhumbertohernandez@gmail.com>
2020-06-09 17:32:23 +0300

    expose public resources

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const server = express();
 const { PORT } = require("./config");
 
+server.use(express.static("./public"));
+
 server.listen(PORT, () => {
   console.log(`Application running on PORT ${PORT}`);
 });
