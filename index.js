3230ee09a43d884f20f68dc8974ab774fb7d06ea
FHHF <fabianhumbertohernandez@gmail.com>
2020-06-09 17:37:28 +0300

    project MVC scaffold

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const server = express();
 const { PORT } = require("./config");
 
 server.use(express.static("./public"));
+server.use(express.json)
 
 server.listen(PORT, () => {
   console.log(`Application running on PORT ${PORT}`);
